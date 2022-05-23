b8fce674835c2c7ac6c2c927bc3b7756bc441409
Daniel Kogut <danieltugok@gmail.com>
2022-05-22 20:49:04 -0500

    starting project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import styles from './App.module.css';
+import poweredImage from './assets/powered.png';
 
-function App() {
+const App = () => {
+  const [heightField, setHeightField] = useState<number>(0)
+  const [weightField, setWeightField] = useState<number>(0)
+  const handleCalculeButton = () => {
+    if(heightField && weightField){
+      const imc = weightField / (heightField * heightField)
+      alert(imc)
+    } else alert("Digite todos os campos.")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={styles.main}>
+      <header>
+        <div className={styles.headerContainer}>
+          <img src={poweredImage} alt="" width={150} />
+        </div>
       </header>
+      <div className={styles.container}>
+      <div className={styles.leftSide}>
+        <h1>Calcule your IMC</h1>
+        <p>IMC e a sigla qpara indice de massa coporea, parametro adotado pela XXX...</p>
+        
+        <input
+          type="number"
+          placeholder="Digite a sua altura. Ex: 1.5 (Em metros)"
+          value={heightField > 0 ? heightField : ''}
+          onChange={e => setHeightField(parseFloat(e.target.value))}
+        />
+
+        <input
+          type="number"
+          placeholder="Digite o seu peso. Ex: 75.3 (Em Kg)"
+          value={weightField > 0 ? weightField : ''}
+          onChange={e => setWeightField(parseFloat(e.target.value))}
+        />
+
+        <button onClick={handleCalculeButton}>Calcular</button>
+
+        </div>
+      <div className={styles.rightSide}>
+        right
+        </div>
+
+      </div>
+
     </div>
-  );
+  )
+
 }
 
 export default App;
